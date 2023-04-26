283b03c51173fe3691ff2e3098de5f16b5a4c1bc
BrownieMaar <barnamarci07@gmail.com>
2023-04-26 19:33:09 +0200

    implement base skeleton for CantusDaoFirebase.ts

--- src/model/CantusDaoFirebase.ts
+++ src/model/CantusDaoFirebase.ts
@@ -1,7 +1,15 @@
 import {CantusDao} from "./CantusDao";
 import {CantusData} from "./types/CantusTypes";
+import {Firestore, getFirestore} from "firebase/firestore";
+import {FirebaseApp} from "firebase/app";
 
 export class CantusDaoFirebase implements CantusDao {
+    db: Firestore;
+
+    constructor(app: FirebaseApp) {
+        this.db = getFirestore(app);
+    }
+
     addNewCantus(cantusData: CantusData, userId: string): Promise<boolean> {
         return Promise.resolve(false);
     }
