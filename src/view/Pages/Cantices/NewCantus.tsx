6d65a13489cd5de694f83d01681b33f20895eb0c
BrownieMaar <barnamarci07@gmail.com>
2023-04-26 18:41:41 +0200

    implement onSave function in NewCantus

--- src/view/Pages/Cantices/NewCantus.tsx
+++ src/view/Pages/Cantices/NewCantus.tsx
@@ -1,13 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { CantusData } from "../../../model/types/CantusTypes";
 import CantusEditor from "../../Components/CantusEditor";
+import { DatabaseContext } from "../../App";
+import { useContext } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { UserContext } from "../../App";
 
 
 export default function NewCantus() {
     const navigate = useNavigate();
+    const db = useContext(DatabaseContext);
+    const user = useContext(UserContext);
     
-    const onSave = (cantus: CantusData) => {}
+    const onSave = (cantus: CantusData) => {
+        if (!user) return
+        db.addNewCantus(cantus, user.uid).then(() => navigate(-1))
+    }
     const onCancel = () => navigate(-1)
 
-    return <CantusEditor onSave={onSave} onCancel={onCancel} />
+    return <CantusEditor onSave={onSave} onCancel={onCancel} loggedIn={!!user} />
 }
