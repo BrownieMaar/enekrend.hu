450e20440956170d117774a3786c327262c21fe8
BrownieMaar <barnamarci07@gmail.com>
2023-04-26 18:09:20 +0200

    implement onCancel function

--- src/view/Pages/Cantices/NewCantus.tsx
+++ src/view/Pages/Cantices/NewCantus.tsx
@@ -1,11 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { CantusData } from "../../../model/types/CantusTypes";
 import CantusEditor from "../../Components/CantusEditor";
 
 
 export default function NewCantus() {
+    const navigate = useNavigate();
     
     const onSave = (cantus: CantusData) => {}
-    const onCancel = () => {}
+    const onCancel = () => navigate(-1)
 
     return <CantusEditor onSave={onSave} onCancel={onCancel} />
 }
