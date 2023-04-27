3cc144bc972a0499fd51a593d038f9e2e89b380e
BrownieMaar <barnamarci07@gmail.com>
2023-04-27 10:14:44 +0200

    add new methods to dao with new dto and implement getting one by docId

--- src/model/CantusDao.ts
+++ src/model/CantusDao.ts
@@ -1,5 +1,8 @@
 import {CantusData} from "./types/CantusTypes";
+import {CantusDto} from "./types/Dto";
 
 export interface CantusDao {
-    addNewCantus(cantusData: CantusData, userId: string): Promise<boolean>;
+    addNewCantus(cantusData: CantusData, userId: string): Promise<string>;
+    getCantusByDocId(docId: string): Promise<CantusDto>;
+    getCanticesWithUserIdAndTimestamp(): Promise<CantusDto[]>
 }
--- src/model/CantusDaoFirebase.ts
+++ src/model/CantusDaoFirebase.ts
@@ -1,7 +1,8 @@
 import {CantusDao} from "./CantusDao";
 import {CantusData} from "./types/CantusTypes";
-import {Firestore, getFirestore} from "firebase/firestore";
+import {addDoc, collection, doc, Firestore, getDoc, getFirestore, serverTimestamp} from "firebase/firestore";
 import {FirebaseApp} from "firebase/app";
+import {CantusDto} from "./types/Dto";
 
 export class CantusDaoFirebase implements CantusDao {
     db: Firestore;
@@ -10,7 +11,39 @@ export class CantusDaoFirebase implements CantusDao {
         this.db = getFirestore(app);
     }
 
-    addNewCantus(cantusData: CantusData, userId: string): Promise<boolean> {
-        return Promise.resolve(false);
+    async addNewCantus(cantusData: CantusData, userId: string): Promise<string> {
+        try {
+            const docRef = await addDoc(collection(this.db, "cantus"), {
+                cantusData: cantusData,
+                userId: userId,
+                created: serverTimestamp()
+            })
+            console.log("Cantus written with ID: ", docRef.id);
+            return docRef.id
+        } catch (e) {
+            console.log("Error adding cantus: ", e);
+            throw e;
+        }
     }
+
+    async getCantusByDocId(docId: string): Promise<CantusDto> {
+
+        const docRef = doc(this.db, "cantus", docId);
+
+        const docSnap = await getDoc(docRef);
+        if (!docSnap.exists()) throw new Error("No cantus with docId " + docId + " found");
+        return {
+            docId: docSnap.id,
+            userId: docSnap.data().userId,
+            created: docSnap.data().created.toDate(),
+            cantusData: docSnap.data().cantusData,
+        };
+
+    }
+
+    getCanticesWithUserIdAndTimestamp(): Promise<CantusDto[]> {
+        return Promise.resolve([]);
+    }
+
+
 }
