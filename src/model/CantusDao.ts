a08d4fb5ea0b6b54ae7097b21cd483d8b64b28e2
BrownieMaar <barnamarci07@gmail.com>
2023-04-30 08:59:38 +0200

    add method to cantusDao to get versionDtos to cantus

--- src/model/CantusDao.ts
+++ src/model/CantusDao.ts
@@ -1,9 +1,10 @@
 import {CantusData} from "./types/CantusTypes";
-import {CantusDto} from "./types/Dto";
+import {CantusDto, CantusVersionDto} from "./types/Dto";
 
 export interface CantusDao {
     addNewCantus(cantusData: CantusData, userId: string): Promise<string>;
     addNewCantusVersion(cantusData: CantusData, userId: string): Promise<string>;
-    getCantusById(uniqueId: string): Promise<CantusDto>;
+    getCantusById(uniqueId: string, versionId?: string): Promise<CantusDto>;
+    getCantusVersionDTOsById(uniqueId: string): Promise<CantusVersionDto[]>;
     getCanticesWithUserIdAndTimestamp(): Promise<CantusDto[]>
 }
