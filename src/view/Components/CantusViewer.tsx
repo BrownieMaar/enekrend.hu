57e10ab9dd045a1d96d7758e6236e7544a6896da
BrownieMaar <barnamarci07@gmail.com>
2023-04-30 07:36:11 +0200

    add cantusviewer page

---
+++ src/view/Components/CantusViewer.tsx
@@ -0,0 +1,66 @@
+import {Cantus, CantusData, GenreOptionsWithLabels, ToneOptionsWithLabels} from "../../model/types/CantusTypes";
+import {Box, Divider, Stack, Typography, useMediaQuery, useTheme} from "@mui/material";
+import {CantusImpl} from "../../controller/CantusImpl";
+import {useEffect, useState} from "react";
+import {CantusRenderer} from "../../controller/CantusRenderer";
+
+export default function CantusViewer({cantusData}: { cantusData: CantusData }) {
+    const isMoreThanLg = useMediaQuery(useTheme().breakpoints.up('lg'));
+    const [containerWidth, setContainerWidth] = useState(document.getElementById("cantus-component-container")?.clientWidth ?? 200)
+    const cantus: Cantus = new CantusImpl(cantusData)
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = document.getElementById("cantus-component-container")?.clientWidth;
+            if (width) setContainerWidth(width);
+        }
+        window.addEventListener('resize', handleResize)
+        handleResize();
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, []);
+
+    return <Stack direction={isMoreThanLg ? "row" : "column"} spacing={2}>
+
+        <Stack spacing={2} flexGrow={1}>
+            <Typography variant={"body1"}>
+                <b>Genre:</b> {GenreOptionsWithLabels.find(genre => cantus.genre === genre.value)?.label ??
+                <i>unknown</i>}
+            </Typography>
+            <Typography variant={"body1"}>
+                <b>Cantus ID:</b> {cantus.cantusId ?? <i>unknown</i>}
+            </Typography>
+            <Typography variant={"body1"}>
+                <b>Tone:</b> {ToneOptionsWithLabels.find(tone => cantus.tone === tone.value)?.label + "." ??
+                <i>unknown</i>}
+            </Typography>
+            <Typography variant={"body1"}>
+                <b>Bible quote:</b> {
+                cantus.bibleQuote && cantus.bibleQuote.length !== 0
+                    ? cantus.bibleQuote.map((bibleQuote) => {
+                        return `${bibleQuote.book} ${bibleQuote.startChapter},${bibleQuote.startVerse}-${bibleQuote.endChapter},${bibleQuote.endVerse}`
+                    }).join("; ")
+                    : <i>unknown</i>
+            }
+            </Typography>
+            {
+                cantus.notes
+                    ? <Typography variant={"body1"}>
+                        <b>Notes:</b> {cantus.notes ?? <i>unknown</i>}
+                    </Typography>
+                    : undefined
+            }
+        </Stack>
+
+        <Divider orientation={isMoreThanLg ? "vertical" : "horizontal"} flexItem/>
+
+        <Box
+            flexGrow={1}
+            id={"cantus-component-container"}
+        >
+            <CantusRenderer width={containerWidth} cantus={cantus}/>
+        </Box>
+
+    </Stack>
+}
