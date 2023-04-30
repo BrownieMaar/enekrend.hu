d752b388096a6a8b9ad1a9765052a48f26758cc8
BrownieMaar <barnamarci07@gmail.com>
2023-04-30 10:53:01 +0200

    design versions menu and add query param option to load version

--- src/view/Pages/Cantices/CantusPage.tsx
+++ src/view/Pages/Cantices/CantusPage.tsx
@@ -1,16 +1,33 @@
-import {useNavigate, useParams} from "react-router-dom";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
 import React, {useContext, useEffect, useState} from "react";
 import {CantusData} from "../../../model/types/CantusTypes";
 import {DatabaseContext, UserContext} from "../../App";
 import CantusEditor from "../../Components/CantusEditor";
-import {Button, Menu, MenuItem, Paper, Stack, ToggleButton, ToggleButtonGroup} from "@mui/material";
+import {
+    Avatar,
+    Button,
+    Menu,
+    MenuItem,
+    MenuList,
+    Paper,
+    Stack,
+    ToggleButton,
+    ToggleButtonGroup,
+    Typography
+} from "@mui/material";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import CantusViewer from "../../Components/CantusViewer";
 import {CantusVersionDto} from "../../../model/types/Dto";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {UserDao} from "../../../model/UserDao";
+import {AppUser} from "../../../model/types/UserTypes";
 
-const ViewEditPicker = ({editing, setEditing}: {editing: boolean, setEditing:  React.Dispatch<React.SetStateAction<boolean>>}) => <ToggleButtonGroup size={"small"} value={editing} color={"primary"}>
+const ViewEditPicker = ({
+                            editing,
+                            setEditing
+                        }: { editing: boolean, setEditing: React.Dispatch<React.SetStateAction<boolean>> }) =>
+    <ToggleButtonGroup size={"small"} value={editing} color={"primary"}>
         <ToggleButton value={false} onClick={() => setEditing(false)}>
             <VisibilityIcon fontSize={"small"}/>
         </ToggleButton>
@@ -19,7 +36,17 @@ const ViewEditPicker = ({editing, setEditing}: {editing: boolean, setEditing:  R
         </ToggleButton>
     </ToggleButtonGroup>
 
-const VersionPicker = ({versions, versionId, setVersionId}: {versions: CantusVersionDto[], versionId: string | undefined, setVersionId: React.Dispatch<React.SetStateAction<string | undefined>>}) => {
+const VersionPicker = ({
+                           versions,
+                           versionId,
+                           setVersionId,
+                           userDb
+                       }: {
+    versions: CantusVersionDto[],
+    versionId: string | undefined,
+    setVersionId: React.Dispatch<React.SetStateAction<string | undefined>>,
+    userDb: UserDao,
+}) => {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -35,17 +62,51 @@ const VersionPicker = ({versions, versionId, setVersionId}: {versions: CantusVer
             aria-haspopup="true"
             aria-expanded={open ? "true" : undefined}
             onClick={handleClick}
-            endIcon={<ExpandMoreIcon />}
+            endIcon={<ExpandMoreIcon/>}
             variant={"outlined"}
         >
             Versions
         </Button>
-        <Menu open={open} anchorEl={anchorEl} onClose={handleClose} id="versions-menu" aria-labelledby="versions-button">
-            {versions.map(version => <MenuItem key={version.docId} onClick={() => {
-                setVersionId(version.docId)
-                handleClose()
-            }}>{version.created.toISOString()}
-            </MenuItem>)}
+        <Menu open={open} anchorEl={anchorEl} onClose={handleClose} id="versions-menu"
+              aria-labelledby="versions-button">
+            <MenuList>
+                {versions.map(version => {
+                    const [user, setUser] = useState<AppUser | undefined>(undefined)
+
+                    useEffect(() => {
+                        userDb.getAppUserData(version.userId).then(user => {
+                            setUser(user)
+                        })
+                    }, [version.userId])
+
+                    return <MenuItem
+                        key={version.docId}
+                        onClick={() => {
+                            setVersionId(version.docId)
+                            handleClose()
+                        }}
+                        selected={versionId === version.docId}
+                    >
+                        <Stack direction={"row"} spacing={2}>
+
+                            <Avatar
+                                alt={user?.displayName || undefined}
+                                src={user?.photoUrl || undefined}
+                            >
+                                {user?.displayName?.charAt(0) || ""}
+                            </Avatar>
+                            <Stack>
+                            <Typography variant={"body2"}>
+                                {version.created.toLocaleDateString() + " " + version.created.toLocaleTimeString()}
+                            </Typography>
+                                <Typography variant={"caption"}>
+                                    {user?.displayName || "Unknown"}
+                                </Typography>
+                            </Stack>
+                        </Stack>
+                    </MenuItem>
+                })}
+            </MenuList>
         </Menu>
     </>
 }
@@ -55,7 +116,7 @@ export default function CantusPage() {
     const user = useContext(UserContext);
     const {id} = useParams();
     const [cantusData, setCantusData] = useState<CantusData | undefined>(undefined);
-    const [versionId, setVersionId] = useState<string | undefined>(undefined);
+    const [versionId, setVersionId] = useState<string | undefined>(new URLSearchParams(useLocation().search).get("versionId") || undefined);
     const [versions, setVersions] = useState<CantusVersionDto[]>([]);
     const [editing, setEditing] = useState(false);
     const navigate = useNavigate();
@@ -103,9 +164,9 @@ export default function CantusPage() {
     return cantusData
         ? <>
             <Stack direction={"row"} gap={2} justifyContent={"flex-end"} sx={{marginBlock: 2}}>
-                    <VersionPicker versions={versions} versionId={versionId} setVersionId={setVersionId}/>
+                <VersionPicker versions={versions} versionId={versionId} setVersionId={setVersionId} userDb={db.user}/>
                 {user ? <ViewEditPicker editing={editing} setEditing={setEditing}/> : <></>}
-                </Stack>
+            </Stack>
 
             {
                 user && editing
