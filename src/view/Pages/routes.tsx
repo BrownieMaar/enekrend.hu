71c138628c3324cad5bd6f66f748d469d214f101
BrownieMaar <barnamarci07@gmail.com>
2023-05-24 09:34:33 +0200

    Add NewLiturgy game and add it to routes

--- src/view/Pages/routes.tsx
+++ src/view/Pages/routes.tsx
@@ -2,21 +2,17 @@ import Cantices from "./Cantices/Cantices";
 import CantusPage from "./Cantices/CantusPage";
 import NewCantus from "./Cantices/NewCantus";
 import Home from "./Home/Home";
+import NewLiturgy from "./Liturgies/NewLiturgy";
 import Profile from "./Profile/Profile";
 
-export const ROUTES = [
-    {
-        path: '/',
-        element: <Home />,
-        mainPath: false,
-        name: "Home"
-    },
-    {
-        path: '/profile/:uid',
-        element: <Profile />,
-        mainPath: false,
-        name: "Profile"
-    },
+type Route = {
+    path: string;
+    element: JSX.Element;
+    mainPath: boolean;
+    name: string;
+}
+
+const CANTUS_ROUTES: Route[] = [
     {
         path: '/cantices',
         element: <Cantices />,
@@ -36,6 +32,33 @@ export const ROUTES = [
         name: "CantusPage"
     },
 ]
+
+const LITURGY_ROUTES: Route[] = [
+    {
+        path: "/liturgy/new",
+        element: <NewLiturgy />,
+        mainPath: false,
+        name: "NewLiturgy",
+    },
+]
+
+export const ROUTES: Route[] = [
+    {
+        path: '/',
+        element: <Home />,
+        mainPath: false,
+        name: "Home"
+    },
+    {
+        path: '/profile/:uid',
+        element: <Profile />,
+        mainPath: false,
+        name: "Profile"
+    },
+    ...CANTUS_ROUTES,
+    ...LITURGY_ROUTES,
+]
+
 
 export const setWindowTitle = (title: string) => {
     document.title = title
