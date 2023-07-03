1c08b8fd97817be16096f747b35cf2775199bb1d
BrownieMaar <barnamarci07@gmail.com>
2023-07-03 21:28:26 +0200

    fix dialog map keylessness

--- src/view/Components/LiturgyPartCards/Dialog.tsx
+++ src/view/Components/LiturgyPartCards/Dialog.tsx
@@ -2,15 +2,15 @@ import { Typography } from "@mui/material";
 import { Dialogus } from "../../../model/types/RecitableTypes";
 import { getStringFromTBSA } from "../../../controller/recitableTools";
 
-export default function DialogCard({part}: {part: Dialogus}) {
+export default function DialogCard({ part }: { part: Dialogus }) {
 
     return <div>
-    <Typography variant="h6">Dialog</Typography>
-    {part.contents.map(verse => 
-    <div style={{marginTop: ".5rem"}}>
-    <Typography variant="body1"><b>V.</b> {getStringFromTBSA(verse.versus)}</Typography>
-    <Typography variant="body1"><b>R.</b> {getStringFromTBSA(verse.responsum)}</Typography>
-    </div>
+        <Typography variant="h6">Dialog</Typography>
+        {part.contents.map((verse, index) =>
+            <div key={`dialog ${part.uniqueId} verse ${index}`} style={{ marginTop: ".5rem" }}>
+                <Typography variant="body1"><b>V.</b> {getStringFromTBSA(verse.versus)}</Typography>
+                <Typography variant="body1"><b>R.</b> {getStringFromTBSA(verse.responsum)}</Typography>
+            </div>
         )}
-</div>
+    </div>
 }
