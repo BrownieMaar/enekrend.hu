e8b844e700a80757c4d53b6984fde758a911e98a
BrownieMaar <barnamarci07@gmail.com>
2023-07-03 21:28:45 +0200

    implement basic psalmus card

--- src/view/Components/LiturgyPartCards/Psalm.tsx
+++ src/view/Components/LiturgyPartCards/Psalm.tsx
@@ -1,6 +1,17 @@
+import { Typography } from "@mui/material";
+import { getStringFromTBSA } from "../../../controller/recitableTools";
 import { Psalmus } from "../../../model/types/RecitableTypes";
 
-export default function PsalmCard({part}: {part: Psalmus}) {
+export default function PsalmCard({ part }: { part: Psalmus }) {
 
-    return <>Psalm</>
+    return <div>
+        <Typography variant="h6">Dialog</Typography>
+        {part.contents.map((verse, index) =>
+            <div key={`psalmus ${part.uniqueId} verse ${index}`} style={{ marginTop: ".5rem" }}>
+                {verse.flexa && <Typography variant="body1">{getStringFromTBSA(verse.flexa)}&nbsp;+</Typography>}
+                <Typography variant="body1">{getStringFromTBSA(verse.mediatio)}&nbsp;*</Typography>
+                <Typography variant="body1">{getStringFromTBSA(verse.terminatio)}</Typography>
+            </div>
+        )}
+    </div>
 }
