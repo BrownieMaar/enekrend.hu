f11e275feda4e0d89f920e21c8635cbc7a636fb3
BrownieMaar <barnamarci07@gmail.com>
2023-07-05 08:29:35 +0200

    make psalm title more sophisticated on card

--- src/view/Components/LiturgyPartCards/Psalm.tsx
+++ src/view/Components/LiturgyPartCards/Psalm.tsx
@@ -3,9 +3,16 @@ import { getStringFromTBSA } from "../../../controller/recitableTools";
 import { Psalmus } from "../../../model/types/RecitableTypes";
 
 export default function PsalmCard({ part }: { part: Psalmus }) {
+    const getTitle = () => {
+        if (part.title) {
+            if (part.genre === "Psalm") return "Psalmus " + part.title
+            return part.title
+        }
+        part.genre === "Psalm" ? "Psalmus" : "Canticum"
+    }
 
     return <div>
-        <Typography variant="h6">{part.title ||( part.genre === "Cant" ? "Canticum" : "Psalm")}</Typography>
+        <Typography variant="h6">{getTitle()}</Typography>
         {part.contents.map((verse, index) =>
             <div key={`psalmus ${part.uniqueId} verse ${index}`} style={{ marginTop: ".5rem" }}>
                 {verse.flexa && <Typography variant="body1">{getStringFromTBSA(verse.flexa)}&nbsp;†</Typography>}
