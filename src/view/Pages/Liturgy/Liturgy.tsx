67b6f0f3cd07f1d68effd28c232e2811450d2a19
BrownieMaar <barnamarci07@gmail.com>
2023-07-05 08:30:22 +0200

    connect liturgy and newLiturgy to db

--- src/view/Pages/Liturgy/Liturgy.tsx
+++ src/view/Pages/Liturgy/Liturgy.tsx
@@ -1,7 +1,37 @@
-import { useParams } from "react-router-dom"
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
+import { LiturgyData } from "../../../model/types/LiturgyTypes";
+import { UserContext, DatabaseContext } from "../../App";
+import LiturgyEditor from "../../Components/LiturgyEditor";
 
 export default function Liturgy() {
+    const [liturgy, setLiturgy] = useState<LiturgyData | null>(null)
     const { id } = useParams()
+    const user = useContext(UserContext)
+    const db = useContext(DatabaseContext)
+    const navigate = useNavigate();
+    const onSave = (liturgy: LiturgyData) => {
+        if (!user) return
+        db.liturgy.uploadLiturgy(liturgy, user.uid).then((id) => {
+            db.liturgy.getLiturgyById(id).then(liturgyDto => {
+                console.log(liturgyDto)
+            })
+        }
+        ).catch(err => console.log(err))
+    }
+    const onCancel = () => navigate("/liturgies");
 
-    return <>Liturgy {id}</>
+    useEffect( () => {
+        if (!id) return
+        db.liturgy.getLiturgyById(id).then(liturgy => {
+            console.log("finished getting liturgy")
+            console.log(liturgy)
+            setLiturgy(liturgy.liturgyData)
+        }
+        ).catch(err => console.log(err))
+    }, []);
+
+    return liturgy === null 
+    ? <div>Loading...</div>
+    : <LiturgyEditor onSave={onSave} onCancel={onCancel} liturgyData={liturgy} loggedIn={!!user} />
 }
--- src/view/Pages/Liturgy/NewLiturgy.tsx
+++ src/view/Pages/Liturgy/NewLiturgy.tsx
@@ -1,8 +1,23 @@
+import { useContext } from "react"
+import { DatabaseContext, UserContext } from "../../App"
 import LiturgyEditor from "../../Components/LiturgyEditor"
+import { useNavigate } from "react-router-dom";
+import { LiturgyData } from "../../../model/types/LiturgyTypes";
 
 export default function NewLiturgy() {
-    const onSave = () => {}
-    const onCancel = () => {}
-    
-    return <LiturgyEditor onSave={onSave} onCancel={onCancel} />
+    const user = useContext(UserContext)
+    const db = useContext(DatabaseContext)
+    const navigate = useNavigate();
+    const onSave = (liturgy: LiturgyData) => {
+        if (!user) return
+        db.liturgy.uploadLiturgy(liturgy, user.uid).then((id) => {
+            db.liturgy.getLiturgyById(id).then(liturgyDto => {
+                console.log(liturgyDto)
+            })
+        }
+        ).catch(err => console.log(err))
+    }
+    const onCancel = () => navigate("/liturgies");
+
+    return <LiturgyEditor onSave={onSave} onCancel={onCancel} loggedIn={!!user} />
 }
